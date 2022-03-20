3cdf43cd59e3bbf41ef6f9e23eb0ee906554c339
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-20 10:15:34 +0530

    Custom Exception

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -18,8 +18,12 @@ namespace MoodAnalyserProblem
         {
             try
             {
+                if (message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
+                }
 
-                if (message.ToLower().Contains("happy"))
+                else if (message.ToLower().Contains("happy"))
                 {
                     return "happy";
                 }
